1a22fdd853244bd4864e6ae164991fae9b78b35b
NikBel523 <belyakov.nkt@gmail.com>
2024-07-21 11:49:50 +0300

    feat: модель пользователя зарегистрирована в админке, есть возможность администратору добавить нового пользователя.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import MetroUser
+
+
+@admin.register(MetroUser)
+class MetroUserAdmin(UserAdmin):
+    """Администратор пользователей."""
+
+    list_display = (
+        'email',
+        'full_name',
+    )
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('email', 'full_name', 'avatar', 'address')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'email', 'full_name', 'avatar', 'address'),
+        }),
+    )
+
+    search_fields = (
+        'email',
+        'full_name',
+    )
