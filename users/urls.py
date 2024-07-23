431ca1fc0be147efab61d9c4cb5c19a8f85c3b13
NikBel523 <belyakov.nkt@gmail.com>
2024-07-23 23:39:06 +0300

    feat: добавлена главная страница, сделана view функция, прописан url, сделан html шаблон.

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MetroUserView
+from .views import MetroUserView, user_home
 
 
 app_name = 'users'
 
 urlpatterns = [
     path('users/', MetroUserView.as_view(), name='users_list'),
+    path('users/home', user_home, name='user_home'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
-from django.views.generic import ListView
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import DetailView, ListView
 
 from users.models import MetroUser
+
+User = get_user_model()
 
 
 class MetroUserView(ListView):
@@ -9,3 +13,15 @@ class MetroUserView(ListView):
     model = MetroUser
     template_name = 'users.html'
     context_object_name = 'users'
+
+
+def user_home(request):
+    """Генерация домашней страницы пользователя."""
+    print(request.user)
+    user = get_object_or_404(User, id=request.user.id)
+
+    return render(
+        request,
+        'home.html',
+        context={'profile': user},
+    )
